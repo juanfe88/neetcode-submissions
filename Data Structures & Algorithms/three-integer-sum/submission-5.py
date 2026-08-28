09895227115695047a7413565faf229c083bb6f6
Juan Felipe Arias <52606138+juanfe88@users.noreply.github.com>
2026-08-28 09:54:07 +0200

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        resps = []
+        seen = set()
+        def twosum(array,target):
+            complements = {}
+            seen_pairs = set()
+            if len(array)<2:
+                return
+            for idx,val in enumerate(array):
+                complement = target - val
+                pair_idx = complements.get(val,None)
+                if pair_idx is not None:
+                    triplet = [-target,array[pair_idx],val]
+                    if tuple(triplet) not in seen_pairs:
+                        resps.append(triplet)
+                        seen_pairs.add(tuple(triplet))
+                complements[complement] = idx
+        
+        for idx,num in enumerate(nums):
+            if num in seen:
+                continue
+            twosum(nums[idx+1:],-num)
+            seen.add(num)
+
+        return resps    
+        
